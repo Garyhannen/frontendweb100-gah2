45a7786699e00f61fabb642eede54a93ca9f435f
Garyhannen <gahannen@cox.net>
2019-03-14 14:04:12 +0000

    first break of day 3

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,11 @@
+describe('writing a Spec', () => {
+    it('is easey when you know how', () => {
+        // given 
+        const a = 10, b = 20;
+        // when
+        const answer = a + b;
+        //then - 
+        expect(answer).toBe(30);
+    });
+
+});
